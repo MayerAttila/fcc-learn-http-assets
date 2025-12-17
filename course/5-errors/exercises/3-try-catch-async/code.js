e11ba4995838e8f49044f2ef5bc68851a882e1fe
MayerAttila <mayerattila4@gmail.com>
2025-12-17 14:25:46 +0100

    finished async, errors and headers part

--- course/5-errors/exercises/3-try-catch-async/code.js
+++ course/5-errors/exercises/3-try-catch-async/code.js
@@ -1,9 +1,13 @@
-const leaderboard = await fetchLeaderBoard()
-console.log(leaderboard)
+try {
+  const leaderboard = await fetchLeaderBoard();
+  console.log(leaderboard);
+} catch (error) {
+  console.log("Failed to fetch leaderboard: " + error);
+}
 
 // don't touch below this line
 
 async function fetchLeaderBoard() {
-  const response = await fetch('https://fantasyquest.servers')
-  return response.json()
+  const response = await fetch("https://fantasyquest.servers");
+  return response.json();
 }
