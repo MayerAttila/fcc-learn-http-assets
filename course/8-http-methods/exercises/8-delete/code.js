607a10800bcc63d5fa859c17fee92d15e17cd379
MayerAttila <mayerattila4@gmail.com>
2025-12-18 16:52:19 +0100

    complerted methods part of the course

--- course/8-http-methods/exercises/8-delete/code.js
+++ course/8-http-methods/exercises/8-delete/code.js
@@ -1,49 +1,57 @@
 async function deleteUser(baseURL, id, apiKey) {
-  const fullURL = `${baseURL}/${id}`
-  // ?
+  const fullURL = `${baseURL}/${id}`;
+  const response = fetch(fullURL, {
+    method: "DELETE",
+    mode: "cors",
+    headers: {
+      "X-API-Key": apiKey,
+    },
+  });
 }
 
 // don't touch below this line
 
-const userId = '0194fdc2-fa2f-4cc0-81d3-ff12045b73c8'
-const generatedKey = generateKey()
-const url = 'https://api.boot.dev/v1/courses_rest_api/learn-http/users'
+const userId = "0194fdc2-fa2f-4cc0-81d3-ff12045b73c8";
+const generatedKey = generateKey();
+const url = "https://api.boot.dev/v1/courses_rest_api/learn-http/users";
 
 async function getUsers(url, apiKey) {
   const response = await fetch(url, {
-    method: 'GET',
-    mode: 'cors',
+    method: "GET",
+    mode: "cors",
     headers: {
-      'X-API-Key': apiKey
-    }
-  })
-  return response.json()
+      "X-API-Key": apiKey,
+    },
+  });
+  return response.json();
 }
 
 function generateKey() {
-  const characters = 'ABCDEF0123456789'
-  let result = ''
+  const characters = "ABCDEF0123456789";
+  let result = "";
   for (let i = 0; i < 16; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length))
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
-  return result
+  return result;
 }
 
 function logUsers(users) {
-  console.log("Logging user records:")
+  console.log("Logging user records:");
   for (const user of users) {
-    console.log(`User uuid: ${user.id}, Character name: ${user.characterName}, Class: ${user.class}, Level: ${user.level}, User: ${user.user.name}`)
+    console.log(
+      `User uuid: ${user.id}, Character name: ${user.characterName}, Class: ${user.class}, Level: ${user.level}, User: ${user.user.name}`
+    );
   }
 }
 
-const users = await getUsers(url, generatedKey)
-logUsers(users)
-console.log('---')
+const users = await getUsers(url, generatedKey);
+logUsers(users);
+console.log("---");
 
-await deleteUser(url, userId, generatedKey)
-console.log(`Deleted user with id: ${userId}`)
-console.log('---')
+await deleteUser(url, userId, generatedKey);
+console.log(`Deleted user with id: ${userId}`);
+console.log("---");
 
-const newUsers = await getUsers(url, generatedKey)
-logUsers(newUsers)
-console.log('---')
+const newUsers = await getUsers(url, generatedKey);
+logUsers(newUsers);
+console.log("---");
